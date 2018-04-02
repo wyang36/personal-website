af4b550b1c7345e0f3b62fe716a40cb82747ef1c
wyang36 <wenqin.z.yang@gmail.com>
2018-04-02 18:53:56 -0400

    css updates
    for intro page

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -4,7 +4,7 @@ import classes from './Intro.css';
 const Intro = () => (
     <div className={classes.Intro}>
         <h1>Hi! I'm Kira.</h1>
-        <h2>I'm a front-end software engineer and amateur rock climber.</h2>
+        <h2>I'm a front-end software engineer and amateur rock climber currently based in Atlanta GA.</h2>
     </div>
 );
 
