15dc7320700639c79f02c6aae9d1966a51686bc8
wyang36 <kira@knock.com>
2021-01-28 18:00:48 -0800

    text changes and typo fixes

--- src/components/About/Experiences/Experiences.js
+++ src/components/About/Experiences/Experiences.js
@@ -24,7 +24,7 @@ const experiences = () => {
         time: 'Jan 2014 - July 2018',
         description: `This is my first job out of college and I was in a team responsible for rewriting the biggest product
          to a more modern looking single page application using newer technologies. Because of this, I learned about all sorts of technologies used throughout the whole stack
-         and experimented with a bunch more. In 2017 I self-taught VueJS and lead the company wide effort to rewrite the entire application for the second time. 
+         and experimented with a bunch more. In 2017 I self-taught VueJS and led the company wide effort to rewrite the entire application for the second time. 
          I was promoted to senior engineer afterwards.`
     }
 
@@ -35,7 +35,7 @@ const experiences = () => {
         employer: 'Panasonic Automotive Systems of America',
         role: 'Tools Software Co-op',
         time: 'Jan - May 2013',
-        description: `This is my last co-op term during my time at Georgia Tech. I assisted in the programming of the GUIs used for tesing. My biggest project was to design and
+        description: `This is my last co-op term during my time at Georgia Tech. I assisted in the programming of the GUIs used for testing. My biggest project was to design and
         program buttons in the GUI to mimic the ones on a car radio, so the testers can control the radio from their computers.
         I learned a lot of Windows Forms and had a lot of fun!`
     }
--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -4,7 +4,7 @@ import classes from './Intro.css';
 const intro = () => (
     <div className={classes.Intro}>
         <h1 className={classes.FadeIn}>Hi! I'm <span>Kira</span>.</h1>
-        <h2 className={classes.FadeIn}>I'm a full-stack <span>software engineer</span> and amateur rock climber based in Atlanta GA.</h2>
+        <h2 className={classes.FadeIn}>I'm a full-stack <span>software engineer</span> and amateur rock climber based in Los Angeles CA.</h2>
     </div>
 );
 
